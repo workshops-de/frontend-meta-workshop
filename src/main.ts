d758f75289dfd53635612f3221c5ad8560eb70d2
Björn Wilmsmann <bjoern@bjoernkw.com>
2022-10-25 15:44:11 +0200

    Explicitly adding TypeScript and tslib dependencies for CodeSandbox

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import 'bootstrap/scss/bootstrap-utilities.scss';
 import 'bootstrap/scss/bootstrap.scss';
 import '@fortawesome/fontawesome-free/css/all.css';
 
